29560247ac9e7bb0a315fefa43a1624272ab2d32
NoreenNaz1234 <38943389+NoreenNaz1234@users.noreply.github.com>
2018-10-18 17:48:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+  });
+
+   M.toast({html: 'I am a toast!'})
